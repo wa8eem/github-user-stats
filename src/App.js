a1967f712a6c6b434af9685b33d748ba6b4c32d0
wa8eem <muhammadwaseem220@gmail.com>
2023-12-17 23:50:40 +0100

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
+      <Router>
+        <Dashboard></Dashboard>
+      </Router>
+      <Router>
+        <Login />
+      </Router>
     </div>
   );
 }
