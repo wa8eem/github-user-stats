21297ff5dd0de7efb5ca7c33ecca340ccc54f268
wa8eem <muhammadwaseem220@gmail.com>
2023-12-18 23:34:36 +0100

    Fixed the error: The user doesn't login and redirects to the login page again!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from "react";
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-        <PrivateRoute path="/" exact={true}> 
-          <Dashboard></Dashboard>
-        </PrivateRoute>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path='*'>
-          <Error></Error>
-        </Route>
-        </Switch>
-      </Router>
+      <AuthWrapper>
+        <Router>
+          <Switch>
+            <PrivateRoute path='/' exact={true}>
+              <Dashboard></Dashboard>
+            </PrivateRoute>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='*'>
+              <Error></Error>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthWrapper>
     </div>
   );
 }
