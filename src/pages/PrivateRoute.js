4b0da05e39d42cbd970add20af8f297a100210de
wa8eem <muhammadwaseem220@gmail.com>
2023-12-18 23:26:25 +0100

    Login Button Tries to Authenticate the User Now

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -4,8 +4,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children, ...rest }) => {
   const { isAuthenticated, user } = useAuth0();
-  // const isUser = isAuthenticated && user;
-  const isUser=true; //mimic that use has logged in
+  const isUser = isAuthenticated && user;
   return (
     <Route
       {...rest}
