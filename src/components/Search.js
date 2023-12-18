5429c7c23db98d043a2cabdd7f2e21338c1af0bf
wa8eem <muhammadwaseem220@gmail.com>
2023-12-18 18:02:00 +0100

    Determine the number of remaining requests of the IP address from GitHub API

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user, setUser] = React.useState('');
-
+  const { requests } = React.useContext(GithubContext);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
@@ -22,9 +22,10 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
+            {requests > 0 && <button type='submit'>search</button>}
           </div>
         </form>
-        <h3>requests : 60 / 60</h3>
+        <h3>requests : {requests} / 60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -15,8 +15,27 @@ const GithubProvider = ({ children }) => {
   // githubuser has the data, setGithubUser is gonna be used to the set the data
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  //  check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          // toggleError(true, 'sorry, you have exceeded your hourly rate limit!');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+   // error
+   useEffect(checkRequests, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
