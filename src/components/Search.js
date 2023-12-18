52489dfb9dfa5ad9474442d779c960f472a0fb6c
wa8eem <muhammadwaseem220@gmail.com>
2023-12-18 17:03:07 +0100

    Whatever you enter in the search box can be retreived, and user set to that value

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,31 @@ import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <MdSearch />
+            <input
+              type='text'
+              placeholder='enter github user'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </div>
+        </form>
+        <h3>requests : 60 / 60</h3>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
