44d7dfc26e86ae924d06a039f2bcc8a7d76b6306
wa8eem <muhammadwaseem220@gmail.com>
2023-12-18 14:17:38 +0100

    Passed a stateful Mocked data instead of 'hello' as an object using context

--- src/context/context.js
+++ src/context/context.js
@@ -11,8 +11,14 @@ const GithubContext = React.createContext();
 // Provider, Consumer - GithubContext.Provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  // githubuser has the data, setGithubUser is gonna be used to the set the data
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 export { GithubProvider, GithubContext };
